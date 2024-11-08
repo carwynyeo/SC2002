13d65ff984e77845a60af490d285927299180b70
Malcolm Fong <167612961+malcolmfong01@users.noreply.github.com>
2024-11-08 10:12:59 +0800

    fix: import java.time libraries

--- Hospital Management System/src/com/hospitalmanagementsystem/Utility/DateTime.java
+++ Hospital Management System/src/com/hospitalmanagementsystem/Utility/DateTime.java
@@ -1,9 +1,11 @@
 package com.hospitalmanagementsystem.Utility;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class DateTime {
     public static LocalDateTime parseDateTime(String dateTime) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         return LocalDateTime.parse(dateTime, formatter);
     }
 }
-
