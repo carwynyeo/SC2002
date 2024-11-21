17f4d89249968e2a14dd9dc4a4ec4afbfecba441
Carwyn Yeo Ruizhi <144221886+carwynyeo@users.noreply.github.com>
2024-11-21 23:19:05 +0800

    Add files via upload

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"boundary"},{"l":"controller"},{"l":"enums"},{"l":"Main"},{"l":"model"},{"l":"model.userInterfaces"},{"l":"repository"},{"l":"utility"}];updateSearchResults();
